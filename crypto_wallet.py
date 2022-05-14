6e821bb327817697907cecd2cc26ae26ec0f85c4
mikecordes <81245089+mikecordes@users.noreply.github.com>
2022-05-14 10:48:20 -0400

    Add files via upload
    adding working files

---
+++ crypto_wallet.py
@@ -0,0 +1,71 @@
+# Cryptocurrency Wallet
+################################################################################
+
+# This file contains the Ethereum transaction functions for the NPS_NFT_Finder.py
+
+################################################################################
+# Imports
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+from bip44 import Wallet
+from web3 import Account
+from web3 import middleware
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+
+################################################################################
+# Wallet functionality
+
+def generate_account():
+     # Fetch mnemonic
+    mnemonic = os.getenv("MNEMONIC")
+
+    # Create Wallet Object
+    wallet = Wallet(mnemonic)
+
+    # Derive Ethereum Private Key
+    private, public = wallet.derive_account("eth")
+
+    # Convert private key into an Ethereum account
+    account = Account.privateKeyToAccount(private)
+
+    return account
+
+def get_balance(w3, address):
+    # Get balance of address in Wei
+    wei_balance = w3.eth.get_balance(address)
+
+    # Convert Wei value to ether
+    ether = w3.fromWei(wei_balance, "ether")
+
+    # Return the value in ether
+    return ether
+
+
+def send_transaction(w3, account, to, total_donation ):
+    """Send an authorized transaction to the Ganache blockchain."""
+    # Set gas price strategy
+    w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
+
+    # Convert eth amount to Wei
+    value = w3.toWei(total_donation, "ether")
+
+    # Calculate gas estimate
+    gasEstimate = w3.eth.estimateGas({"to": to, "from": account.address, "value": value})
+
+    # Construct a raw transaction
+    raw_tx = {
+        "to": to,
+        "from": account.address,
+        "value": value,
+        "gasPrice": 0,
+        "gas": gasEstimate,
+        "nonce": w3.eth.getTransactionCount(account.address)
+    }
+
+    # Sign the raw transaction with ethereum account
+    signed_tx = account.signTransaction(raw_tx)
+
+    # Send the signed transactions
+    return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
